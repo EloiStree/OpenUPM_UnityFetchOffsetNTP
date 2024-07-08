7102d00f021d9a56bade0175b3e391c2d41e8fd5
Eloi Stree <eloistree@gmail.com>
2024-07-08 04:46:32 +0200

    Save: 07/08/2024 4:46 AM

---
+++ Runtime/NtpOffsetTickAverageMono.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NtpOffsetTickAverageMono : MonoBehaviour
+{
+    public NtpOffsetTickAverage m_offsetAverage;
+    public UnityEvent<long> m_onOffsetTickChanged;
+
+    public void PushNewOffset(long tickOffset)
+    {
+        m_offsetAverage.PushNewOffset(tickOffset);
+        m_onOffsetTickChanged.Invoke(m_offsetAverage.GetAverage());
+    }
+}
+
+
+
+
+[System.Serializable]
+public class NtpOffsetTickAverage
+{
+
+    public long m_lastOffsetTickReceived;
+    public long m_minimumOffsetTick;
+    public long m_maximumOffsetTick;
+    public long m_averageOffsetTick;
+
+    private static long m_lastAverageOffsetTick;
+    public static long GetLastAverageOffsetTickComputed()
+    {
+        return m_lastAverageOffsetTick;
+    }
+
+    public int m_historySize = 10;
+    public List<long> m_historyOffsetTicks = new List<long>();
+    public void PushNewOffset(long tickOffset)
+    {
+        if (m_minimumOffsetTick == 0)
+        {
+            m_minimumOffsetTick = tickOffset;
+        }
+        if (m_maximumOffsetTick == 0)
+        {
+            m_maximumOffsetTick = tickOffset;
+        }
+        if (m_averageOffsetTick == 0)
+        {
+            m_averageOffsetTick = tickOffset;
+        }
+
+        m_lastOffsetTickReceived = tickOffset;
+        m_historyOffsetTicks.Add(tickOffset);
+        if (m_historyOffsetTicks.Count > m_historySize)
+        {
+            m_historyOffsetTicks.RemoveAt(0);
+        }
+        if (m_minimumOffsetTick > tickOffset)
+        {
+            m_minimumOffsetTick = tickOffset;
+        }
+        if (m_maximumOffsetTick < tickOffset)
+        {
+            m_maximumOffsetTick = tickOffset;
+        }
+        m_averageOffsetTick = GetAverage();
+        m_lastAverageOffsetTick = m_averageOffsetTick;
+    }
+
+    public long GetAverage()
+    {
+        long sum = 0;
+        foreach (var offset in m_historyOffsetTicks)
+        {
+            sum += offset;
+        }
+        return sum / m_historyOffsetTicks.Count;
+    }
+}
